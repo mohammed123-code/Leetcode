3294a49be6900b9dc60f3c6937a07580116e2f69
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-05-30 15:43:58 +0530

    3606-MINIMUM-ELEMENT-AFTER-REPLACEMENT-WITH-DIGIT-SUM Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int func(int a){
-        int sum=0;
-        while(a!=0){
-            int rem=a%10;
-            sum+=rem;
-            a=a/10;
-        }
-        return sum;
-    }
     int minElement(vector<int>& nums) {
+        string number="";
         int n=nums.size();
-
-        int mini=INT_MAX;
         for(int i=0;i<n;i++){
-            if(func(nums[i])<mini){
-                mini=func(nums[i]);
-            }
+            number+=(to_string(nums[i]));
+            number+="-";
         }
-        return mini;
+        
+
+        for(int i=0;i<number.size();i++){
+            int mini=0;
+            if(number[i]!='-'){}
+        }
     }
 };
