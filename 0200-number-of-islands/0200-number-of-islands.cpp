9b95a463f545d4b9c9e2169e99b4e2c729950d84
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-07-21 10:33:27 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 25 ms (72.3%), Space: 16.2 MB (80.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int r,int c){
-        if( r<0 || c<0 || r>=grid.size() || c>=grid[0].size()  ||  grid[r][c]=='0'){
+
+    void func(vector<vector<char>>& grid, int r, int c){
+        int n=grid.size();
+        int m=grid[0].size();
+
+        //base case
+        if(r<0 || c<0 || r>=n || c>=m){
             return;
         }
-        grid[r][c]='0';
-        dfs(grid,r-1,c);
-        dfs(grid,r+1,c);
-        dfs(grid,r,c-1);
-        dfs(grid,r,c+1);
-    }
+        if(grid[r][c]=='0'){
+            return;
+        }
 
+        grid[r][c]='0';
+
+        func(grid,r-1,c);
+        func(grid,r+1,c);
+        func(grid,r, c-1);
+        func(grid,r, c+1);
+
+    }
     int numIslands(vector<vector<char>>& grid) {
         int count=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
+
+        int n=grid.size();
+        int m=grid[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j);
+                    func(grid,i,j);
                 }
             }
         }
-    
-    return count;
+
+        return count;
     }
 };
