826c205821ee68af4b7388df5a06f96f4b4a7b50
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-09-01 22:25:06 +0530

    0130-SURROUNDED-REGIONS Stats: Time: 0 ms (100%), Space: 14.6 MB (47.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,70 +1,71 @@
 class Solution {
 public:
-    // Standard DFS function to explore and mark connected 'O's
-    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>& visited) {
-        int row = board.size();
-        int col = board[0].size();
-        
-        // Mark current cell as visited
-        visited[r][c] = 1;
-        
-        // 4 Directional arrays: Up, Right, Down, Left
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
-        
-        for (int i = 0; i < 4; i++) {
-            int nrow = r + delRow[i];
-            int ncol = c + delCol[i];
-            
-            // Check boundary conditions, unvisited status, and if it's an 'O'
-            if (nrow >= 0 && nrow < row && ncol >= 0 && ncol < col 
-                && !visited[nrow][ncol] && board[nrow][ncol] == 'O') {
-                dfs(nrow, ncol, board, visited);
+
+
+    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>& visited){
+        int row=board.size();
+        int col=board[0].size();
+
+        visited[r][c]=1;
+
+        int ri[]={-1, 0, 1, 0};
+        int ci[]={0, 1, 0, -1};
+
+
+        for(int i=0;i<4;i++){
+            int nr=r+ri[i];
+            int nc=c+ci[i];
+
+            if(nr>=0 && nr<row && nc>=0 && nc<col && (!visited[nr][nc]) && board[nr][nc]=='O'){
+                dfs(nr,nc,board,visited);
             }
         }
     }
 
+
     void solve(vector<vector<char>>& board) {
-        if (board.empty()) return;
+        if(board.empty()) return;
+
+        int row=board.size();
+        int col=board[0].size();
+
+        vector<vector<int>>visited(row, vector<int>(col,0));
         
-        int row = board.size();          
-        int col = board[0].size();       
-        vector<vector<int>> visited(row, vector<int>(col, 0));
 
-        // top boundary -> Trigger DFS if 'O' found
-        for(int i = 0; i < col; i++){
-            if(!visited[0][i] && board[0][i] == 'O'){
-                dfs(0, i, board, visited);
+
+        //top
+        for(int i=0;i<col;i++){
+            if(!visited[0][i] && board[0][i]=='O'){
+                dfs(0,i,board,visited);
             }
         }
 
-        // bottom boundary -> Trigger DFS if 'O' found
-        for(int i = 0; i < col; i++){
-            if(!visited[row - 1][i] && board[row - 1][i] == 'O'){
-                dfs(row - 1, i, board, visited);
+        //down
+        for(int i=0;i<col;i++){
+            if(!visited[row-1][i] && board[row-1][i]=='O'){
+                dfs(row-1,i,board,visited);
             }
         }
 
-        // left boundary -> Trigger DFS if 'O' found
-        for(int i = 0; i < row; i++){
-            if(!visited[i][0] && board[i][0] == 'O'){
-                dfs(i, 0, board, visited);
+        //left
+        for(int i=0;i<row;i++){
+            if(!visited[i][0] && board[i][0]=='O'){
+                dfs(i,0,board,visited);
             }
         }
 
-        // right boundary -> Trigger DFS if 'O' found
-        for(int i = 0; i < row; i++){
-            if(!visited[i][col - 1] && board[i][col - 1] == 'O'){
-                dfs(i, col - 1, board, visited);
+        //right
+        for(int i=0;i<row;i++){
+            if(!visited[i][col-1] && board[i][col-1]=='O'){
+                dfs(i,col-1,board,visited);
             }
         }
 
-        // Final Step: Flip captured regions
-        // If an 'O' is not visited, it is not connected to a boundary -> Flip to 'X'
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(!visited[i][j] && board[i][j] == 'O') {
-                    board[i][j] = 'X';
+        //final
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(!visited[i][j] && board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
