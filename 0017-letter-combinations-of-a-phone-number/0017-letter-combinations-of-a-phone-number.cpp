4e1f76dfd268234befd5c647dfc07631b79818a3
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-09-07 22:43:57 +0530

    0017-LETTER-COMBINATIONS-OF-A-PHONE-NUMBER Stats: Time: 3 ms (5.01%), Space: 10.2 MB (5.6%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-
-    void func(int index, string digits, string keypad[], string res, vector<string>& ans) {
-        if(index == digits.size()){
-            if(!res.empty()){
-                ans.push_back(res);
+    void func(int index, string digits, vector<string>numpad, string ans, vector<string>&res){
+        if(ans.size()==digits.size()){
+            if(ans.size()!=0){
+                res.push_back(ans);
             }
             return;
         }
-
-        int n = digits[index] - '0';
-        string s1 = keypad[n];
-        for(int i = 0; i < s1.size(); i++){
-            func(index + 1, digits, keypad, res + s1[i], ans);
+        int n=digits[index]-'0';
+        string str=numpad[n];
+        for(int i=0;i<str.size();i++){
+            func(index+1, digits, numpad, ans+str[i], res);
         }
     }
-
     vector<string> letterCombinations(string digits) {
-        if(digits.empty()) return {};
-        string keypad[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        vector<string> ans;
-        func(0, digits, keypad, "", ans);
-        return ans;
+        if(digits.empty()){
+            return {};
+        }
+        vector<string>numpad={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string>res;
+        int index=0;
+        string ans="";
+        func(index, digits, numpad, ans, res);
+        return res;
     }
 };
