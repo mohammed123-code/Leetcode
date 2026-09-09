3f1937c6c0a3866dbbcc4bc29b86ca976e54ed51
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-09-09 09:57:55 +0530

    0907-KOKO-EATING-BANANAS Stats: Time: 4 ms (86.55%), Space: 23 MB (58.75%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
 
-    bool canFinish(vector<int>& piles, int h, int k){
-        long long hours=0;  //should not exceed the h
+    bool canFinish(vector<int>& piles, int h, int n){
+        long tot=0;
         for(int i=0;i<piles.size();i++){
-            hours+=piles[i]/k;
-            if(piles[i]%k!=0){
-                hours++;
+            tot+=(piles[i]/n);
+            if(piles[i]%n!=0){
+                tot++;
             }
-            if(hours>h){
-                return false;
-            }
+        }
+        if(tot>h){
+            return false;
         }
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left=1;
-        int right=*max_element(piles.begin(), piles.end());
-        int res;
+        int low=1;
+        int high=*max_element(piles.begin(), piles.end());
+        int res=high;
 
-        while(left<=right){
-            int mid=left+(right-left)/2;
-
+        while(low<=high){
+            int mid=low+(high-low)/2;
             if(canFinish(piles, h, mid)){
                 res=mid;
-                right=mid-1;
+                high=mid-1;
             }
             else{
-                left=mid+1;
+                low=mid+1;
             }
         }
+
         return res;
     }
 };
