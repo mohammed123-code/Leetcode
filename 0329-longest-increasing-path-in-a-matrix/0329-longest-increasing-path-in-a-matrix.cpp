14f19d4e5c1ec86f46ee18381f480945236169ee
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-09-10 20:58:30 +0530

    0329-LONGEST-INCREASING-PATH-IN-A-MATRIX Stats: Time: 6 ms (84.83%), Space: 20.8 MB (74.94%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,61 +1,39 @@
 class Solution {
 public:
+    int dr[4]={-1,1,0,0};
+    int dc[4]={0,0,-1,1};
+    int dfs(vector<vector<int>>& matrix, int m , int n, int i, int j, vector<vector<int>>&memo){
+        if(memo[i][j]!=-1){   //if i ahve already the value, just take it, dont do again
+            return memo[i][j];
+        }
+        int maxi=0;
+        for(int k=0;k<4;k++){
+            int ni=i+dr[k];
+            int nj=j+dc[k];
+            if(ni>=0 && nj>=0 && ni<m && nj<n && matrix[i][j]<matrix[ni][nj]){
+                int len=dfs(matrix, m, n, ni, nj, memo);
+                maxi=max(maxi, len);
+            }
+        }
+        memo[i][j]=maxi+1;
+        return maxi+1;  //add one for current pos
+    }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
+        //lets try using DFS and memo as we got 
+        // TIME LIMIT EXCEEDED so lets add memo table
+        
         int m=matrix.size();
         int n=matrix[0].size();
+        vector<vector<int>>memo(m ,vector<int>(n, -1));  //---->this is the memo table
 
-        vector<vector<int>>indegree(m, vector<int>(n,0));
 
-        int dr[4]={-1,1,0,0};
-        int dc[4]={0,0,-1,1};
-
+        int maxipath=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                for(int k=0;k<4;k++){
-                    int ni=i+dr[k];
-                    int nj=j+dc[k];
-
-                    if(ni>=0 && nj>=0 && ni<m && nj<n && matrix[i][j]<matrix[ni][nj]){
-                        indegree[ni][nj]++;
-                    }
-                }
+                int path=dfs(matrix, m, n, i, j, memo);
+                maxipath=max(maxipath, path);
             }
         }
-
-
-        queue<pair<int, int>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(indegree[i][j]==0){
-                    q.push({i,j});
-                }
-            }
-        }
-
-
-        int res=0;
-        while(!q.empty()){
-            int size=q.size();
-            res++;
-
-            while(size--){
-                auto [i, j] =q.front();
-                q.pop();
-
-
-                for(int k=0;k<4;k++){
-                    int ni=i+dr[k];
-                    int nj=j+dc[k];
-
-                    if(ni>=0 && nj>=0 && ni<m && nj<n && matrix[i][j]<matrix[ni][nj]){
-                        indegree[ni][nj]--;
-                        if(indegree[ni][nj]==0){
-                            q.push({ni,nj});
-                        }
-                    }
-                }
-            }
-        }
-        return res;
+        return maxipath;
     }
 };
