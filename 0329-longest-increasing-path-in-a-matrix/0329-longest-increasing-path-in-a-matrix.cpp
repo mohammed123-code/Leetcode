0073753676e96c7247348e67fa623115b23726aa
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-09-10 20:41:35 +0530

    0329-LONGEST-INCREASING-PATH-IN-A-MATRIX Stats: Time: 23 ms (24.01%), Space: 22.7 MB (25.47%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        //converting the matrix to a graph
         int m=matrix.size();
         int n=matrix[0].size();
-        
-        //insted of creating the new graph, we create the indegree matrix for it
+
         vector<vector<int>>indegree(m, vector<int>(n,0));
 
-        //all four directions
-        int dr[4]={-1, 1, 0, 0};
-        int dc[4]={0, 0, -1, 1};
-
+        int dr[4]={-1,1,0,0};
+        int dc[4]={0,0,-1,1};
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -26,16 +22,7 @@ public:
             }
         }
 
-        // //see the indegree for understanding
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<indegree[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
 
-
-        //Hye if u have the indegree 0, go to the queue
         queue<pair<int, int>>q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -46,26 +33,13 @@ public:
         }
 
 
-
-
-        int len=0;  //-->this is the output variable
-
-
-        //now the   KAHN's Algoo..
+        int res=0;
         while(!q.empty()){
             int size=q.size();
-            len++;
+            res++;
 
             while(size--){
-
-                
-                pair<int,int> node=q.front();
-                int i=node.first;
-                int j=node.second;
-
-                //or  auto {i, j}= q.front();
-
-
+                auto [i, j] =q.front();
                 q.pop();
 
 
@@ -73,27 +47,15 @@ public:
                     int ni=i+dr[k];
                     int nj=j+dc[k];
 
-                    if(ni>=0 && nj>=0 && ni<m && nj<n && matrix[ni][nj]>matrix[i][j]){
+                    if(ni>=0 && nj>=0 && ni<m && nj<n && matrix[i][j]<matrix[ni][nj]){
                         indegree[ni][nj]--;
                         if(indegree[ni][nj]==0){
-                        q.push({ni, nj});
+                            q.push({ni,nj});
+                        }
                     }
-                    }
-                    
                 }
             }
         }
-        
-
-
-        
-
-
-
-   return len;
-
-
-
+        return res;
     }
 };
-
