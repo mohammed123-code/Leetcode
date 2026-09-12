d5ceb55e6bfe46616094a4791a724a7133040da8
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-09-12 16:38:53 +0530

    4245-COUNT-COMMAS-IN-RANGE Stats: Time: 0 ms (100%), Space: 8.7 MB (14.48%) - LeetHub

--- 4245-count-commas-in-range/4245-count-commas-in-range.cpp
+++ 4245-count-commas-in-range/4245-count-commas-in-range.cpp
@@ -4,12 +4,6 @@ public:
         if(n<1000){
             return 0;
         }
-
-        // if(n>=1000 && n<=99999){
-            
-        // }
-        // else if(1000000)
-
-        return n-1000+1;
+        return n-999;
     }
 };
