a4003372765805edeac0fa4c41b484441ed554b6
Santhu Mohammed M <afsalsandhu@gmail.com>
2026-09-18 21:36:18 +0530

    0329-LONGEST-INCREASING-PATH-IN-A-MATRIX Stats: Time: 17 ms (33.18%), Space: 20.7 MB (74.72%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -2,38 +2,37 @@ class Solution {
 public:
     int dr[4]={-1,1,0,0};
     int dc[4]={0,0,-1,1};
-    
-    int dfs(int r, int c, int m, int n, vector<vector<int>>& matrix, vector<vector<int>>&memo){
-        if(memo[r][c]!=-1){
-            return memo[r][c];
+    int dfs(int i, int j, vector<vector<int>>&matrix, int m, int n, vector<vector<int>>&memo){
+        if(memo[i][j]!=0){
+            return memo[i][j];
         }
         int maxi=0;
         for(int k=0;k<4;k++){
-            int nr=r+dr[k];
-            int nc=c+dc[k];
-
-            if(nr>=0 && nc>=0 && nr<m && nc<n  ){
-                if(matrix[nr][nc]>matrix[r][c]){
-                    int len=dfs(nr, nc, m, n, matrix, memo);
-                    maxi=max(maxi, len);
-                }
+            int nr=i+dr[k];
+            int nc=j+dc[k];
+            
+            if(nr>=0 && nc>=0 && nr<m && nc<n && matrix[i][j]<matrix[nr][nc]){
+                int len=dfs(nr, nc, matrix, m, n, memo);
+                maxi=max(len, maxi);
             }
-
         }
-        memo[r][c]=maxi+1;
+        memo[i][j]=maxi+1;
         return maxi+1;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<vector<int>>memo(m, vector<int>(n,-1));
+
+        vector<vector<int>>memo(m, vector<int>(n,0));
+
         int maxi=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int len=dfs(i,j,m,n,matrix,memo);
-                maxi=max(maxi, len);              
+                
+                    int len=dfs(i, j, matrix, m, n, memo);
+                    maxi=max(len, maxi);
+                
             }
-            
         }
         return maxi;
     }
